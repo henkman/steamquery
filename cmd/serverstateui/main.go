18a3f6cec0500be68acc19035838a78116f24c1a
henkman <henkman@github.com>
2017-11-16 14:15:47 +0100

    serverstateui: now without http server

--- cmd/serverstateui/main.go
+++ cmd/serverstateui/main.go
@@ -1,161 +1,71 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"log"
-	"net"
 	"os"
 	"path/filepath"
+	"strconv"
 
-	"github.com/buaazp/fasthttprouter"
 	"github.com/henkman/steamquery"
-	"github.com/valyala/fasthttp"
 	"github.com/zserge/webview"
 )
 
-func Players(ctx *fasthttp.RequestCtx) {
-	ctx.Response.Header.Set("Content-Type", "application/json")
-	ctx.Response.Header.Set("Cache-Control", "No-Cache")
-	server := ctx.UserValue("server").(string)
-	ps, err := steamquery.QueryPlayersString(server)
-	if err != nil {
-		ctx.Response.SetBodyString(`{"error":"` + err.Error() + `"}`)
-		return
-	}
-	json.NewEncoder(ctx.Response.BodyWriter()).Encode(&ps)
+type Info struct {
+	Name    string
+	Map     string
+	Players int
 }
 
-func Info(ctx *fasthttp.RequestCtx) {
-	ctx.Response.Header.Set("Content-Type", "application/json")
-	ctx.Response.Header.Set("Cache-Control", "No-Cache")
-	server := ctx.UserValue("server").(string)
-	res, err := steamquery.QueryInfoString(server)
-	if err != nil {
-		ctx.Response.SetBodyString(`{"error":"` + err.Error() + `"}`)
-		return
-	}
-	json.NewEncoder(ctx.Response.BodyWriter()).Encode(&res)
+type Server struct {
+	Address string              `json:"address"`
+	Info    Info                `json:"info"`
+	Players []steamquery.Player `json:"players"`
 }
 
-func Index(server string) fasthttp.RequestHandler {
-	return func(ctx *fasthttp.RequestCtx) {
-		ctx.Response.Header.Set("X-UA-Compatible", "IE=edge")
-		ctx.Response.Header.SetContentType("text/html")
-		ctx.Response.SetBodyString(`<html>
-<head>
-	<title>test</title>
-	<style>
-	html, body { margin:0; padding:0; }
-	table.sortable thead {
-	    background-color:#eee;
-	    color:#666666;
-	    font-weight: bold;
-	    cursor: default;
+func (s *Server) UpdatePlayers() {
+	players, err := steamquery.QueryPlayersString(s.Address)
+	if err != nil {
+		s.Players = []steamquery.Player{}
+		return
 	}
-	table.sortable td {
-		padding: 2px;
-	}
-	</style>
-	<script src="/r/sorttable.min.js"></script>
-	<script src="/r/juration.min.js"></script>
-	<script>
-	function updateInfo() {
-		var xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = function() {
-			var DONE = this.DONE || 4;
-			if (this.readyState == DONE) {				
-				var raw = JSON.parse(this.responseText);
-				if(raw.error) {
-					alert(raw.error);
-					return;
-				}
-				document.getElementById("name").innerText = raw.Name;
-				document.getElementById("map").innerText = raw.Map;
-				document.getElementById("playercount").innerText = raw.Players;
-			}
-		};
-		xhr.open("GET", "/info/` + server + `", true);
-		xhr.send();
-	}
-	function updatePlayers() {
-		var xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = function() {
-			var DONE = this.DONE || 4;
-			if (this.readyState == DONE) {			
-				var raw = JSON.parse(this.responseText);
-				if(raw.error) {
-					alert(raw.error);
-					return;
-				}
-				var table = document.querySelector("#players");
-				var tbody = table.querySelector("tbody");
-				tbody.innerHTML = "";
-				for(var i=0; i<raw.length; i++) {
-					var d = raw[i].Duration;
-					var secs = (d/1000000000) + (d%1000000000)/1000000000;
-					var tr = document.createElement("tr");
+	s.Players = players
+}
 
-					var td = document.createElement("td");
-					td.textContent = raw[i].Name;
-					tr.appendChild(td);
-					
-					td = document.createElement("td");
-					td.textContent = raw[i].Score;
-					tr.appendChild(td);
-					
-					td = document.createElement("td");
-					td.textContent = juration.stringify(secs);
-					td.setAttribute("sorttable_customkey", ''+secs);
-					tr.appendChild(td);
-					
-					var td = document.createElement("td");
-					td.textContent = (raw[i].Score/secs).toFixed(6);
-					tr.appendChild(td);
-
-					tbody.appendChild(tr);
-				}
-				var headers = table.querySelectorAll("th");
-				for(var i=0; i<headers.length; i++) {
-					headers[i].className = headers[i].className
-						.replace('sorttable_sorted_reverse','')
-						.replace('sorttable_sorted','');
-				}
-				sorttable.innerSortFunction.apply(headers[3], []);
-				sorttable.reverse(tbody);
-			}
-		};
-		xhr.open("GET", "/players/` + server + `", true);
-		xhr.send();	
+func (s *Server) UpdateInfo() {
+	rules, err := steamquery.QueryRulesString(s.Address)
+	if err != nil {
+		s.Info = Info{}
+		return
 	}
-	window.addEventListener("load", function() {		
-		var buref = document.getElementById("refresh");
-		buref.addEventListener("click", function() {
-			updateInfo();
-			updatePlayers();
-		});
-		buref.click();
-	});
-	</script>
-</head>
-<body>
-	<input type="button" id="refresh" value="refresh"><br/>
-	Name: <span id="name"></span><br/>
-	Map: <span id="map"></span><br/>
-	Players: <span id="playercount"></span><br/>
-	<table id="players" class="sortable">
-		<thead>
-			<tr>
-				<th class="sorttable_alpha">Name</th>
-				<th class="sorttable_numeric">Score</th>
-				<th class="sorttable_numeric">Online</th>
-				<th class="sorttable_numeric">Score/Second</th>
-			</tr>
-		</thead>
-    	<tbody></tbody>
-	</table>
-</body>
-</html>`)
+	var owningPlayerName string
+	var p2 string
+	var numOpenPublicConnections int
+	var numPublicConnections int
+	for _, r := range rules {
+		if r.Name == "OwningPlayerName" {
+			owningPlayerName = r.Value
+		} else if r.Name == "NumOpenPublicConnections" {
+			tmp, err := strconv.Atoi(r.Value)
+			if err != nil {
+				s.Info = Info{}
+				return
+			}
+			numOpenPublicConnections = tmp
+		} else if r.Name == "NumPublicConnections" {
+			tmp, err := strconv.Atoi(r.Value)
+			if err != nil {
+				s.Info = Info{}
+				return
+			}
+			numPublicConnections = tmp
+		} else if r.Name == "p2" {
+			p2 = r.Value
+		}
+	}
+	s.Info = Info{
+		Name:    owningPlayerName,
+		Map:     p2,
+		Players: numPublicConnections - numOpenPublicConnections,
 	}
 }
 
@@ -173,23 +83,22 @@ func main() {
 		flag.Usage()
 		return
 	}
-	exe, err := os.Executable()
+	ex, err := os.Executable()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	ln, err := net.Listen("tcp", "127.0.0.1:0")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer ln.Close()
-	go func() {
-		router := fasthttprouter.New()
-		router.GET("/", Index(_server))
-		router.GET("/players/:server", Players)
-		router.GET("/info/:server", Info)
-		router.ServeFiles("/r/*filepath", filepath.Join(filepath.Dir(exe), "r"))
-		log.Fatal(fasthttp.Serve(ln, router.Handler))
-	}()
-	log.Fatal(webview.Open("server state", "http://"+ln.Addr().String(),
-		600, 800, false))
+	dir := filepath.Dir(ex)
+	url := "file:///" + filepath.ToSlash(dir) + "/r/index.html"
+	w := webview.New(webview.Settings{
+		Title:     "server state",
+		URL:       url,
+		Width:     650,
+		Height:    800,
+		Resizable: true,
+	})
+	defer w.Exit()
+	w.Dispatch(func() {
+		w.Bind("server", &Server{Address: _server})
+	})
+	w.Run()
 }
